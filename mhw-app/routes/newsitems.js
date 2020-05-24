f6d61f1293e292324e1f9b2f6002046b31ac0064
Viktar Chyhir <vchigir12@gmail.com>
2020-05-24 13:31:21 -0400

    Add route and corresponding action for retrieving news items in intervals

--- mhw-app/routes/newsitems.js
+++ mhw-app/routes/newsitems.js
@@ -4,16 +4,23 @@ const { NewsItem } = require('../models/NewsItem')
 const { ObjectID } = require('mongodb')
 
 
-//[GET] - Get all newsitems
-router.get('/getAll', (req, res, next) =>  {
+//[GET] - Get an interval of news items
+router.get('/getInterval', (req, res) =>  {
+    let amnt = req.query.amnt ? parseInt(req.query.amnt, 10) : 0
+    let skipAmnt = req.query.skip ? parseInt(req.query.skip, 10) : 10
+    let left = 0
 
-    NewsItem.find().then((items) => {
-        res.status(200).send(items)
+    NewsItem.countDocuments((err, count) => {
+        if (err) console.log(err)
+        else left = count
+    })
+
+    NewsItem.find().limit(amnt).skip(skipAmnt).then((items) => {
+        res.status(200).send({"items": items, "count": left})
     }).catch((error) => {
-        console.log("Failed endpoint: /newsitem/getAll\n" + error)
+        console.log("Failed endpoint: /newsitem/getInterval\n" + error)
         res.status(500).send()
     })
-    
 })
 
 
---
+++ mhw-app/src/actions/newsitemActions.js
@@ -0,0 +1,31 @@
+
+export const getNewsItemInterval = async (amount, skipAmount) => {
+  const url = `/newsitem/getInterval?amnt=${amount}&skip=${skipAmount}`
+  console.log(url)
+  const request = new Request(url, {
+      method: 'GET', 
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+  });
+
+  try{
+    const res = await fetch(request)
+    if(res.status === 200){
+      let body = await res.json()
+      return {"status": res.status, "items": body.items, "count": body.count}
+    }
+    else{
+      return {"status": res.status}
+    }
+  }
+  catch(err){
+    throw new Error(err)
+  }
+}
+
+
+
+
+
