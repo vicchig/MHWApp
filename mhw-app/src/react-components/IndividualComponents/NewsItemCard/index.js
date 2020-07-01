de77a8a4c47fd419529e097f65efa3847842f145
Viktar Chyhir <vchigir12@gmail.com>
2020-07-01 11:45:36 -0400

    Change text within a news feed card to be contained within a ul

--- mhw-app/src/react-components/IndividualComponents/NewsItemCard/index.js
+++ mhw-app/src/react-components/IndividualComponents/NewsItemCard/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import "./style.css"
+import { uid } from 'react-uid';
 
 class NewsItemCard extends React.Component{
     render(){
-        const {content, date} = this.props
+        const {contents, date} = this.props
+        console.log(contents)
+        const textItems = contents.map((content) => (
+            <li key={uid(content)}>{content}</li>
+        ))
         return(
             <div id="newsItemCard_mainDiv">
                 <div id="newsItemCard_dateDiv">
                     {"Date: " + date}
                 </div>
                 <div id="newsItemCard_contentDiv">
-                    {content}
+                    <ul>{textItems}</ul>
                 </div>
                 
             </div>
