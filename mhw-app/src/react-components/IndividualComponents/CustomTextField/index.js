c54de535f4ee8fd15cd606c05949628fb32519eb
Viktar Chyhir <vchigir12@gmail.com>
2020-07-20 11:29:31 -0400

    Fix CustomTextField losing input after each char input

--- mhw-app/src/react-components/IndividualComponents/CustomTextField/index.js
+++ mhw-app/src/react-components/IndividualComponents/CustomTextField/index.js
@@ -11,10 +11,40 @@ import {
 
 import './style.css'
 
+
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexWrap: 'wrap',
+        '& label.Mui-focused': { //label color for if there is a label
+            color: props => `${props.labelFocusedColour}`,
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: 'green',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': { 
+                borderColor: props => `${props.regularBorderColour}`,//'rgb(164, 164, 164)',
+            },
+            '&:hover fieldset': { 
+                borderColor: props => `${props.hoverBorderColour}`, //'rgb(161, 184, 98)'
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: props => `${props.focusedBorderColour}`,//'rgb(164, 164, 164)',
+                borderWidth: props => `${props.borderWidthFocused}`
+            },
+        },
+        width: props => `${props.width}`,
+        top: props => `${props.top}`,
+        left: props => `${props.left}`,
+        position: props => `${props.position}`,
+        float: props => `${props.float}`,
+        gridColumnStart: props => `${props.gridColStart}`,
+        gridColumnEnd: props => `${props.gridColEnd}`,
+        gridRowStart: props => `${props.gridRowStart}`,
+        gridRowEnd: props => `${props.gridRowEnd}`,
+        alignSelf: props => `${props.alignSelf}`,
+        justifySelf: props => `${props.justifySelf}`,
     },
     margin: {
         margin: theme.spacing(1),
@@ -25,57 +55,26 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function CreateCustomTextField(props) {
-    const classes = useStyles();
+    const classes = useStyles(props);
     const {placeholder, width, top, left, position, float,
            hoverBorderColour, regularBorderColour, focusedBorderColour, label,
            justifySelf, alignSelf, gridRowStart, gridRowEnd, gridColStart, gridColEnd,
-           type, labelFocusedColour, borderWidthFocused
+           type, labelFocusedColour, borderWidthFocused, onChange, value, name, setFocus, onClick
           } = props
-
-    const CustomTextField = withStyles({
-        root: {
-            '& label.Mui-focused': { //label color for if there is a label
-                color: labelFocusedColour,
-            },
-            '& .MuiInput-underline:after': {
-                borderBottomColor: 'green',
-            },
-            '& .MuiOutlinedInput-root': {
-                '& fieldset': { 
-                    borderColor: regularBorderColour,//'rgb(164, 164, 164)',
-                },
-                '&:hover fieldset': { 
-                    borderColor: hoverBorderColour, //'rgb(161, 184, 98)'
-                },
-                '&.Mui-focused fieldset': {
-                    borderColor: focusedBorderColour,//'rgb(164, 164, 164)',
-                    borderWidth: borderWidthFocused
-                },
-            },
-            width: width,
-            top: top,
-            left: left,
-            position: position,
-            float: float,
-            gridColumnStart: gridColStart,
-            gridColumnEnd: gridColEnd,
-            gridRowStart: gridRowStart,
-            gridRowEnd: gridRowEnd,
-            alignSelf: alignSelf,
-            justifySelf: justifySelf,
-
-        }
-       
-    })(TextField);
-
   
     return (
-        <CustomTextField
-          className={classes.margin}
+        <TextField
+          className={classes.root}
           variant="outlined"
           label={label}
           type={type}
+          onChange={onChange}
+          value={value || ""}
+          name={name}
+          autoFocus={setFocus}
+          onClick={onClick}
           id="custom-css-outlined-input"
         />
     );
   }
+
