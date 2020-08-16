efa569ff7ee2ca3a4b985587b95599a9d8ad52af
Viktar Chyhir <vchigir12@gmail.com>
2020-08-16 17:41:00 -0400

    Add search bar with option suggestion functionality

--- mhw-app/src/react-components/Pages/MatsPage/index.js
+++ mhw-app/src/react-components/Pages/MatsPage/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import WebsiteHeader from './../../IndividualComponents/WebsiteHeader'
+import SearchBar from './../../IndividualComponents/SearchBar'
+import {getData} from '../../../actions/dataActions'
 import './style.css'
 
 
 class MatsPage extends React.Component{
 
+    state = {
+        searchbarText: "",
+    }
+
+    handleSearchSelect = (e) => {
+        this.setState({
+            searchbarText: e.value
+        })
+    }
+
     render(){
         return(
             <div id="mainDiv">
                 <WebsiteHeader appContext={this.props.parentContext}/>
-                
+                <div id="searchbarDiv">
+                    <SearchBar id={"searchbar1"} textFieldID={"searchbar"} buttonText={"Add"} searchFunction={getData} searchCategory={"equipmentNames"}      
+                               parentContext={this} dataObjectName={"dataList"} onSetSelect={this.handleSearchSelect}
+                    ></SearchBar>
+                </div>
             </div>
         )
     }
